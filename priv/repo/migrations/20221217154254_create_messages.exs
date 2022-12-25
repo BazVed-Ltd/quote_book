c56546a3612f954cb3c26811fe4827cced232516
Evgeny Rybin <z.ribin20@gmail.com>
2022-12-25 15:57:26 +0500

    Rewrite db migrations

--- priv/repo/migrations/20221217154254_create_messages.exs
+++ priv/repo/migrations/20221217154254_create_messages.exs
@@ -5,17 +5,19 @@ defmodule QuoteBook.Repo.Migrations.CreateMessages do
     create table(:messages) do
       add :quote_id, :integer
       add :text, :string
-      add :peer_id, :integer
-      add :from_id, :integer
+      add :peer_id, references(:chats, on_delete: :nothing)
+      add :from_id, references(:users, on_delete: :nothing)
       add :reply_message_id, references(:messages, on_delete: :nothing)
       add :fwd_from_message_id, references(:messages, on_delete: :nothing)
 
-      add :date, :integer
+      add :date, :bigint
 
       timestamps()
     end
 
     create index(:messages, [:reply_message_id])
     create index(:messages, [:fwd_from_message_id])
+    create index(:messages, [:from_id])
+    create index(:messages, [:peer_id])
   end
 end
--- priv/repo/migrations/20221222075458_add_covers_to_chats.exs
+++
@@ -1,9 +0,0 @@
-defmodule QuoteBook.Repo.Migrations.AddCoversToChats do
-  use Ecto.Migration
-
-  def change do
-    alter table(:chats) do
-      add :covers, {:array, :string}
-    end
-  end
-end
