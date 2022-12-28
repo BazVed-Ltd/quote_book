1b6d006414d6917ef4f76a99adf4831be4b8f6a9
Evgeny Rybin <z.ribin20@gmail.com>
2022-12-29 02:19:47 +0500

    Increment version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule QuoteBook.MixProject do
   def project do
     [
       app: :quote_book,
-      version: "1.0.0",
+      version: "1.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
