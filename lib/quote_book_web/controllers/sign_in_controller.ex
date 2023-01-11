e54c7199fb618dd168b9fd04f1818db7875fd719
Evgeny Rybin <z.ribin20@gmail.com>
2023-01-11 14:02:28 +0500

    Insert user on auth

--- lib/quote_book_web/controllers/sign_in_controller.ex
+++ lib/quote_book_web/controllers/sign_in_controller.ex
@@ -3,12 +3,15 @@ defmodule QuoteBookWeb.SignInController do
 
   def create(conn, %{"token" => silent_token, "uuid" => uuid, "user" => %{"id" => user_id}}) do
     case check_user(uuid, silent_token, user_id) do
-      :ok ->
+      {:ok, user} ->
         {:ok, token, _claims} = QuoteBook.Guardian.encode_and_sign(user_id)
 
         key =
           Guardian.Plug.Keys.token_key(:default)
           |> Atom.to_string()
+
+        user = convert_vk_response_to_db(user)
+        {:ok, _user} = QuoteBook.Book.insert_users([user])
 
         conn
         |> put_resp_cookie(key, token, secure: true, sign: true)
@@ -31,12 +34,26 @@ defmodule QuoteBookWeb.SignInController do
              event: ""
            }
          ) do
-      %{"success" => [%{"user_id" => ^user_id}]} -> :ok
+      %{"success" => [%{"user_id" => ^user_id} = user]} -> {:ok, user}
       _error -> :error
     end
   end
 
   defp app_token do
     Application.get_env(:quote_book, :vk_app_token)
+  end
+
+  defp convert_vk_response_to_db(%{
+         "user_id" => id,
+         "first_name" => first_name,
+         "last_name" => last_name,
+         "photo_100" => current_photo
+       }) do
+        %{
+          id: id,
+          first_name: first_name,
+          last_name: last_name,
+          current_photo: current_photo
+        }
   end
 end
