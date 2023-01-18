6e457c877691f0209b049bd88e426afd4d7d9e69
Evgeny Rybin <z.ribin20@gmail.com>
2023-01-18 17:12:56 +0500

    Amend 339b67368690c9117a6b95e99cd96701393e4c9f

--- mix.exs
+++ mix.exs
@@ -81,7 +81,7 @@ defmodule QuoteBook.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ecto.create", "ecto.migrate"],
       "ecto.seed": ["run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
