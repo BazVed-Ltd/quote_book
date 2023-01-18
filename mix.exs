339b67368690c9117a6b95e99cd96701393e4c9f
Evgeny Rybin <z.ribin20@gmail.com>
2023-01-18 13:51:20 +0500

    Move running seed from `ecto.setup`

--- mix.exs
+++ mix.exs
@@ -82,6 +82,7 @@ defmodule QuoteBook.MixProject do
     [
       setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.seed": ["run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
