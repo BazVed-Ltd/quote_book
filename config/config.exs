d04115de4cfe1cf760b6d8bbba90454b77cf8cf2
Evgeny Rybin <z.ribin20@gmail.com>
2023-01-24 12:05:56 +0500

    Mute compilation warning in elixirls

--- config/config.exs
+++ config/config.exs
@@ -68,6 +68,8 @@ config :quote_book, QuoteBook.Scheduler,
 config :hammer,
   backend: {Hammer.Backend.ETS, [expiry_ms: 60_000 * 60 * 4, cleanup_interval_ms: 60_000 * 10]}
 
+config :quote_book, attachments_directory: "priv/static/attachments"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
