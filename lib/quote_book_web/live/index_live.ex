86c712eda49f9827f519a40a91863706e2ae23cc
Evgeny Rybin <z.ribin20@gmail.com>
2023-01-24 12:46:49 +0500

    Add feed link to `IndexLive`

--- lib/quote_book_web/live/index_live.ex
+++ lib/quote_book_web/live/index_live.ex
@@ -3,7 +3,7 @@ defmodule QuoteBookWeb.IndexLive do
 
   alias QuoteBook.Book
   alias QuoteBook.Book.Chat
-  alias QuoteBookWeb.{ChatLive, SignInLive}
+  alias QuoteBookWeb.{ChatLive, SignInLive, FeedLive}
 
   @impl true
   def mount(_params, _session, socket) do
@@ -27,18 +27,18 @@ defmodule QuoteBookWeb.IndexLive do
     <div class="mt-3 flex flex-col gap-5">
       <div class="card mx-auto px-9">
         <h2 class="text-center text-xl mb-4">Каналы:</h2>
-        <%= if @chats == [] do %>
-          <p>Вы ещё не добавлены ни в какие чаты</p>
-        <% else %>
-          <ul>
-            <%= for chat <- @chats do %>
-              <li>
-                <%= live_redirect chat.title || chat.id,
-                      to: Routes.live_path(assigns.socket, ChatLive, Chat.slug_or_id(chat)) %>
-              </li>
-            <% end %>
-          </ul>
-        <% end %>
+        <ul>
+          <li>
+            <%= live_redirect "Общая лента",
+                    to: Routes.live_path(assigns.socket, FeedLive) %>
+          </li>
+          <%= for chat <- @chats do %>
+            <li>
+              <%= live_redirect chat.title || chat.id,
+                    to: Routes.live_path(assigns.socket, ChatLive, Chat.slug_or_id(chat)) %>
+            </li>
+          <% end %>
+        </ul>
       </div>
       <div class="mx-auto flex flex-col gap-5 items-center">
         <%= if @current_user do %>
